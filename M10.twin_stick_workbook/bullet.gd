7ef0711b4f3d4d83349679715594df2806f1503a
[peeryo] <peeryo@sfusd.edu>
2025-01-21 20:24:33 -0800

    Fixed player movement and bullet firing

--- M10.twin_stick_workbook/bullet.gd
+++ M10.twin_stick_workbook/bullet.gd
@@ -1,4 +1,4 @@
-class_name Bullets extends Area2D
+class_name Bullet extends Area2D
 
 @export var speed := 750
 
@@ -7,16 +7,16 @@ var max_range := 1000.0
 var _traveled_distance = 0.0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physic_process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var distance := speed * delta
 	var motion := Vector2.RIGHT.rotated(rotation) * distance
-	
+
 	position += motion
-	
+
 	_traveled_distance += distance
 	if _traveled_distance > max_range:
 		_destroy()
-	
+
+
 func _destroy():
 	queue_free()
