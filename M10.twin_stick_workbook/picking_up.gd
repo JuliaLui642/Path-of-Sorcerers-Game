88dc86011d9cda3072748148943b164121511a28
JuliaLui642 <159198251+JuliaLui642@users.noreply.github.com>
2025-02-14 15:45:58 -0800

    L11  health pickup Half way through

--- M10.twin_stick_workbook/picking_up.gd
+++ M10.twin_stick_workbook/picking_up.gd
@@ -1,11 +1,32 @@
 class_name Pick_up extends Area2D
 
+@export var item: Item = null: set = set_item
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_item(item)
+	_animation_player.play("idle")
+	body_entered.connected(func(body Node2D)-> void:
+		if body is Player:
+			item.use(body)
+		animation_player.play("destroy")
+		set_deferred("monitoring", false)
+		_audio_stream_player.play()
+		_animation_player.animation_finished.connect(func(_animation_name: String) -> void:
+			queue_free()
+		)
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func set_item(value: Item) -> void:
+	item = value
+	if_sprite2d != null:
+		_sprite2d.texture = item.texture
+	if _audio_stream_player != null:
+		_audio_stream_player.stream = item.sound_on_pickup
