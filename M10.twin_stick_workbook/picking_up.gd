ee1e9d4a027bfede9333f21e9a052c744d0f149a
JuliaLui642 <159198251+JuliaLui642@users.noreply.github.com>
2025-02-24 15:14:41 -0800

    Random update

--- M10.twin_stick_workbook/picking_up.gd
+++ M10.twin_stick_workbook/picking_up.gd
@@ -1,6 +1,6 @@
 class_name Pick_up extends Area2D
 
-@export var item: Item = null: set = set_item
+#@export var item: Item = null: set = set_item
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
@@ -9,24 +9,24 @@ class_name Pick_up extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	set_item(item)
-	_animation_player.play("idle")
-	body_entered.connected(func(body Node2D)-> void:
-		if body is Player:
-			item.use(body)
-		animation_player.play("destroy")
-		set_deferred("monitoring", false)
-		_audio_stream_player.play()
-		_animation_player.animation_finished.connect(func(_animation_name: String) -> void:
-			queue_free()
-		)
+#func _ready() -> void:
+	#set_item(item)
+	#_animation_player.play("idle")
+	#body_entered.connected(func(body Node2D)-> void:
+		#if body is Player:
+			#item.use(body)
+		#animation_player.play("destroy")
+		#set_deferred("monitoring", false)
+		#_audio_stream_player.play()
+		#_animation_player.animation_finished.connect(func(_animation_name: String) -> void:
+			#queue_free()
+		#)
 	
 
 
-func set_item(value: Item) -> void:
-	item = value
-	if_sprite2d != null:
-		_sprite2d.texture = item.texture
-	if _audio_stream_player != null:
-		_audio_stream_player.stream = item.sound_on_pickup
+#func set_item(value: Item) -> void:
+	#item = value
+	#if_sprite2d != null:
+		#_sprite2d.texture = item.texture
+	#if _audio_stream_player != null:
+		#_audio_stream_player.stream = item.sound_on_pickup
